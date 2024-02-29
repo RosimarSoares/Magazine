40c9d38c318fcc71df40d625ba9e136291528c5b
RosimarSoares <160255146+RosimarSoares@users.noreply.github.com>
2024-02-29 17:13:49 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/<Magazine>/",
+  base: "/Magazine/",
   plugins: [],
   resolve: {
     /*something*/
